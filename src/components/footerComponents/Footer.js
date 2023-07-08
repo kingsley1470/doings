60469a6b7bc723309ea2725608dfecf0fb5ce2d5
kingsley <rawmetal77@gmail.com>
2023-07-08 18:28:13 +0200

    update commit

--- src/components/footerComponents/Footer.js
+++ src/components/footerComponents/Footer.js
@@ -6,7 +6,7 @@ const Footer = ()=> {
   return (
     <footer className="footer">
       <div className="footer-content">
-        <p>Contact us at info@example.com</p>
+        <p>Contact us at rawmetal77@gmail.com</p>
         <div className="footer-links">
           <a href="/">
             <FaHome />
@@ -24,7 +24,7 @@ const Footer = ()=> {
         </div>
       </div>
       <div className="footer-bottom">
-        <p>&copy; 2023 Your Website. All rights reserved.</p>
+        <p>&copy; 2023 Kingsley Nwachukwu. All rights reserved.</p>
       </div>
     </footer>
   );
