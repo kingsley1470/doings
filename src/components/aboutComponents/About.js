fef284f4838d0541c0b11e68eb722be0fff83418
kingsley <rawmetal77@gmail.com>
2023-07-16 11:50:24 +0200

    update commit

--- src/components/aboutComponents/About.js
+++ src/components/aboutComponents/About.js
@@ -5,7 +5,7 @@ const About = () => {
   return (
     <div className="about-us">
       <h1>Welcome to Men of Doings!</h1>
-      <p>Men of Doings is an exceptional club formed by 10 young men in Berlin, Germany in 2022. Our founders envisioned a space where men could come together as brothers, united by the common goal of being each other's keeper and providing mutual support. We believe in the power of camaraderie, personal growth, and making a positive impact in our lives and the community around us.</p>
+      <p>Men of Doings is an exceptional club formed by 11 young men in Berlin, Germany in 2022. Our founders envisioned a space where men could come together as brothers, united by the common goal of being each other's keeper and providing mutual support. We believe in the power of camaraderie, personal growth, and making a positive impact in our lives and the community around us.</p>
       <p>At Men of Doings, we foster an environment that encourages personal and professional development. Our club is built on the principles of empowerment, accountability, and brotherhood. Together, we strive to inspire each other to reach our fullest potential and overcome challenges along the way.</p>
       <p>Men of Doings is not just a club; it's a supportive community of like-minded individuals who are committed to each other's success. We believe that by working together, supporting one another, and embracing our shared goals, we can achieve greatness as individuals and as a collective.</p>
       <p>If you're ready to embark on a transformative journey, surrounded by brothers who genuinely care and uplift one another, Men of Doings welcomes you with open arms. Join us today and experience the power of brotherhood!</p>
